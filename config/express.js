12431d2e15e46fb5c0c3894d822ab459045c0dad
o-mago <alexandre.cabral@engenharia.ufjf.br>
2020-09-28 03:58:30 -0300

    no need for require mongodb here

--- config/express.js
+++ config/express.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 
 module.exports = () => {
